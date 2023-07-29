d79efb6e03ee23ab21454f30cc423c2d1c34ab81
debz-g <debz.exe@gmail.com>
2023-07-29 07:28:56 +0530

    Clean Commit #2: - Added Bottom Navigation View - Added Navigation Graph - Added new fragments - Linked Bottom Navigation, Navigation Graph and Fragments together.

--- app/src/main/java/dev/redfox/anisearchclean/presentation/activities/MainActivity.kt
+++ app/src/main/java/dev/redfox/anisearchclean/presentation/activities/MainActivity.kt
@@ -2,6 +2,11 @@ package dev.redfox.anisearchclean.presentation.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import dev.redfox.anisearchclean.R
 import dev.redfox.anisearchclean.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +16,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragmentAnime) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNav.setupWithNavController(navController)
+
+
         setContentView(binding.root)
     }
 }
